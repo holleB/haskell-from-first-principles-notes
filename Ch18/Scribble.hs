b670586f46b18e6716d7cae0a366951af1c4663c
Holger Buchholz <holger@holger.me>
2017-10-24 19:42:08 +0200

    sync

--- Ch18/Scribble.hs
+++ Ch18/Scribble.hs
@@ -1,4 +1,4 @@
-import Control.Monad (join)
+import Control.Monad 
 
 bind :: Monad m => (a -> m b) -> m a -> m b
 bind x = join . fmap x
@@ -10,4 +10,25 @@ twiceWhenEven xs = do
     then [x*x, x*x]
     else []
 
-  
+-- from Chapter Exercises
+j :: Monad m => m (m a) -> m a
+j = join
+
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 = fmap
+
+l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+l2 f x y = f <$> x <*> y
+
+a :: Monad m => m a -> m (a -> b) -> m b
+a = flip (<*>)
+
+meh :: Monad m => [a] -> (a -> m b) -> m [b]
+meh [] _ = return []
+meh (x:xs) f = 
+  let x' = (fmap (\l -> [l]) (f x)) 
+      xs' = meh xs f
+  in (fmap (++) x') <*> xs'
+
+flipType :: (Monad m) => [m a] -> m [a]
+flipType xs = meh xs id
